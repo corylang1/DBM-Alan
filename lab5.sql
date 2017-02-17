74352e70b50eb46205145eed6b0c981a60727ff8
corylang1 <corylang@148-100-141-229.FoxNet.marist.edu>
2017-02-17 13:40:10 -0500

    Lab 5
    Here is my Lab 5. Thanks

---
+++ lab5.sql
@@ -0,0 +1,66 @@
+-- 1 --
+select a.city
+from agents a inner join orders o on a.aid = o.aid
+inner join customers c on o.cid = c.cid
+where c.cid = 'c006'
+;
+ 
+-- 2 --
+select distinct p.pid
+from products p inner join orders o on p.pid = o.pid
+inner join agents a on a.aid = o.aid
+inner join customers c on c.cid = o.cid
+where c.city = 'Kyoto'
+order by pid DESC
+;
+ 
+-- 3 -- 
+Select name
+from customers
+where cid not in(
+    		select cid
+    		from orders
+    		)
+;
+ 
+-- 4 -- 
+select c.name
+from customers c 
+full outer join orders o on c.cid = o.cid
+where o.cid is NULL
+
+;
+ 
+-- 5 --
+select distinct c.name,
+                a.name
+from customers c inner join orders o on c.cid = o.cid
+inner join agents a on a.aid = o.aid
+inner join products p on p.pid = o.pid
+where c.city = a.city
+;
+ 
+-- 6 --
+select c.name as "Customer Name",
+       a.name as "Agent Name",
+       c.city as "City"
+from customers c inner join agents a on c.city = a.city
+
+where c.city = a.city
+;
+ 
+-- 7 --
+
+select city, name
+from customers
+where customers.city in ( 
+
+                        select c.city
+                        from customers c inner join products p on c.city = p.city
+                        group by p.city, 
+                             c.name, 
+                             c.city
+                        limit 1
+                        )
+
+;
